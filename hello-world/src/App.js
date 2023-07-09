2ae855ade9f557919ce2c531dfdb1a10bcf7434f
daemusin12 <engrleonelfelezario812>
2023-07-09 11:08:35 +0800

    Event Handling

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
 // import Counter from './components/Counter';
-import Greet from './components/Greet'
-import Welcome from './components/Welcome';
+// import Greet from './components/Greet'
+// import Welcome from './components/Welcome';
 // import Hello from './components/Hello';
 // import Message from './components/Message';
 
@@ -12,14 +14,16 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Greet name='hotdog' heroName="itlog">
+        {/* <Greet name='hotdog' heroName="itlog">
           <p>This is children props</p>
           <button>Action</button>
         </Greet>
-        <Welcome name='hotdog' heroName="itlog"></Welcome>
+        <Welcome name='hotdog' heroName="itlog"></Welcome> */}
         {/* <Hello></Hello>
         <Message/>
         <Counter /> */}
+        <FunctionClick />
+        <ClassClick />
       </div>
     );
   }
